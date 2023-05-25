3bfa6b23c6e73c06b5afdd1acf5b6526e88e298e
Bonsi <bonsi@protonmail.com>
2023-05-25 09:33:57 +0200

    Add ZSH config

--- install.sh
+++ install.sh
@@ -23,3 +23,9 @@ ln -s "$HOME/dotfiles/xinitrc" "$HOME/.xinitrc"
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
 
+#
+# ZSH 
+################################## 
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/zshenv" "$HOME/.zshenv"
+ln -sf "$HOME/dotfiles/zsh/zshrc" "$HOME/.config/zsh/.zshrc"
